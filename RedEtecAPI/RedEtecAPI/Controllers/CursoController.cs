80d810b842a51c65b69ea9e09ef38951debdbe12
Pruds52 <prudenciopvp@gmail.com>
2024-11-10 15:44:38 -0300

    Criação da CursoController; Criação do endpoint GetCursos

---
+++ RedEtecAPI/RedEtecAPI/Controllers/CursoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedEtecAPI.Entities;
+using RedEtecAPI.Services;
+
+namespace RedEtecAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CursoController : Controller
+    {
+
+        private readonly CursoService _cursoService;
+        private readonly TokenJWTController _tokenJwtController;
+        private readonly UsuarioService _usuarioService;
+
+        public CursoController(CursoService cursoService, TokenJWTController tokenJWTController, UsuarioService usuarioService)
+        {
+            _cursoService = cursoService;
+            _tokenJwtController = tokenJWTController;
+            _usuarioService = usuarioService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetCursos()
+        {
+            var cursos = await _cursoService.GetAllAsync();
+
+            return Ok(cursos);
+        }
+
+    }
+
+}
